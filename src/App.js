38297d4df6ba017879e2bfd446fd2051e71a0ed4
Krishna Madani <krishnamadani@gmail.com>
2024-06-14 02:00:13 -0400

    implemented header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-       
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
