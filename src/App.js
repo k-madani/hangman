2cd0106d4eddbc2f231ee6c105edfc058922a745
k-madani <krishnamadani@gmail.com>
2025-12-18 22:34:54 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -13,11 +13,6 @@ const words = ['application', 'hello', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-let playable = true;
-
-const correctLetters = [];
-const wrongLetters = [];
-
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,23 +1,23 @@
-export function showNotification (setter) {
-    setter(true);
+export function showNotification(setter) {
+  setter(true);
 
-    setTimeout(() => {
-        setter(false);
-    }, 2000)
+  setTimeout(() => {
+    setter(false);
+  }, 2000)
 }
 
-export function checkWin (correct,wrong,word) {
-    let status = 'win';
+export function checkWin(correct, wrong, word) {
+  let status = 'win';
 
-   // Check for win
+  // Check for win
   word.split('').forEach(letter => {
-    if(!correct.includes(letter)){
+    if (!correct.includes(letter)) {
       status = '';
     }
   });
-  
-  // Check for lose
-  if(wrong.length === 6) status = 'lose';
 
-    return status;
+  // Check for lose
+  if (wrong.length === 6) status = 'lose';
+
+  return status;
 }
